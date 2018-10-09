b2d1b2e476567a8dac432f90ed122a5d72ea2244
Nicholas Litwin <nick.litwin@fundbox.com>
2018-10-08 21:51:59 -0700

    Use RepoLinks component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react'
 import './App.css'
-import { fetchCommits } from './api.js'
+import RepoLinks from './components/RepoLinks.js'
+import { fetchCommits, fetchRepositories } from './api.js'
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { username: '', response: null }
+    this.state = {
+      username: '',
+      repos: [],
+      records: [],
+    }
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleUsernameChange = this.handleUsernameChange.bind(this)
+    this.handleUsernameSubmit = this.handleUsernameSubmit.bind(this)
+    this.handleReposSelect = this.handleReposSelect.bind(this)
   }
   
-  handleChange(event) {
+  handleUsernameChange(event) {
     this.setState({ username: event.target.value })
   }
-  
-  async handleSubmit(event) {
-    event.preventDefault();
-    try {
-      await fetchCommits(this.state.username)
-        .then(response => {
-          this.setState({ response })
+
+  handleReposSelect(selectedRepos) {
+    selectedRepos.forEach(repoName => {
+      fetchCommits(this.state.username, repoName)
+        .then(records => {
+          this.setState({
+            records: this.state.records.concat(records)
+          })
         })
+    })
+  }
+  
+  async handleUsernameSubmit(event) {
+    try {
+      const repos = await fetchRepositories(this.state.username)
+      this.setState({ repos })
     } catch(e) {
       console.log(e)
     }
@@ -29,8 +43,8 @@ class App extends Component {
   
   render() {
     let listItems = []
-    if (this.state.response) {
-      listItems = this.state.response.map((record, i) => {
+    if (this.state.records.length) {
+      listItems = this.state.records.map((record, i) => {
         return (
           <li key={i}>
             <a href={record.html_url} target="_blank">{record.sha.slice(0, 7)}</a>
@@ -47,13 +61,11 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">What did I work on?</h1>
         </header>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" name="username" value={this.state.value} onChange={this.handleChange} />
-          <input type="submit" value="Submit" />
-        </form>
-        <ul>
-          {listItems}
-        </ul>
+        <input type="text" name="username" placeholder="Github username" value={this.state.value} onChange={this.handleUsernameChange} />
+        <button type="button" onClick={this.handleUsernameSubmit}>Get repos!</button>
+
+        <RepoLinks repos={this.state.repos.map(({ name }) => name)} onReposSelect={this.handleReposSelect} />
+        {listItems}
       </div>
     );
   }
